ccf234e202c5e119663f415dcb3438486e0f6e31
Rajottam Pradhan <rajottampradhan@gmail.com>
2025-10-07 10:27:45 +0545

    Revert "Created super-admin and bank crud"
    This reverts commit 6080cc97a22b7dea9c949f337f72a15967e2c1f9.

--- system-service/src/main/java/com/ebooks/systemservice/exceptions/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.ebooks.systemservice.exceptions;
-
-public class EmailAlreadyExistsException extends RuntimeException{
-    public EmailAlreadyExistsException(String message){
-        super(message);
-    }
-}
--- system-service/src/main/java/com/ebooks/systemservice/exceptions/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.ebooks.systemservice.exceptions;
-
-public class UsernameAlreadyExistsException extends RuntimeException{
-    public UsernameAlreadyExistsException(String message){
-        super(message);
-    }
-}
